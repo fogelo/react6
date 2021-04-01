74f973f1b61fc57052f24de0856eb1a81e2f7346
fogelo <51332230+fogelo@users.noreply.github.com>
2021-04-01 20:41:07 +0700

    20. Заменил a на Navlink и немного застлизовал их

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,15 @@
 import './Navbar.module.css'
 import classes from './Navbar.module.css'
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <div className={classes.Navbar}>
-            <div className={classes.Item}><a href="/profile">Profile</a></div>
-            <div className={classes.Item}><a href="/dialogs">Messages</a></div>
-            <div className={classes.Item}><a href="/news">News</a></div>
-            <div className={classes.Item}><a href="/music">Music</a></div>
-            <div className={classes.Item}><a href="/settings">Settings</a></div>
+            <div className={classes.Item}><NavLink to="/profile" activeClassName={classes.activeLink}>Profile</NavLink></div>
+            <div className={classes.Item}><NavLink to="/dialogs" activeClassName={classes.activeLink}>Messages</NavLink></div>
+            <div className={classes.Item}><NavLink to="/news" activeClassName={classes.activeLink}>News</NavLink></div>
+            <div className={classes.Item}><NavLink to="/music" activeClassName={classes.activeLink}>Music</NavLink></div>
+            <div className={classes.Item}><NavLink to="/settings" activeClassName={classes.activeLink}>Settings</NavLink></div>
         </div>
 
     )
