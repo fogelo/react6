39f2cd69fe5c2f087e634c50cba49ff07f28356f
fogelo <51332230+fogelo@users.noreply.github.com>
2021-04-02 17:33:37 +0700

    22. Завернул пользователей в тег NavLink

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,33 @@
+import classes from "./Dialogs.module.css"
+import {NavLink} from "react-router-dom";
+
 const Dialogs = () => {
 
-    return  (
-        <div>
-            Dialogs
+    return (
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/1">Jhon</NavLink></div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/2">Sveta</NavLink></div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/3">Mary</NavLink></div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/4">Dima</NavLink></div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/5">Valera</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/6">Viktor</NavLink>
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>How are you?</div>
+                <div className={classes.message}>How are you?</div>
+                <div className={classes.message}>How are you?</div>
+            </div>
         </div>
+
     )
 }
 export default Dialogs;
